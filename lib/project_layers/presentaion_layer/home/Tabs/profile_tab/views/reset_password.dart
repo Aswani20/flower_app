93687746e33c99273c1984225beb507f8a6472e0
lbarsidati22 <lbarsidati575@gmail.com>
2025-09-03 04:16:27 +0000

    fix: loading error in reset password

--- lib/project_layers/presentaion_layer/home/Tabs/profile_tab/views/reset_password.dart
+++ lib/project_layers/presentaion_layer/home/Tabs/profile_tab/views/reset_password.dart
@@ -1,6 +1,7 @@
 import 'package:flower_app/core/di/di.dart';
 import 'package:flower_app/core/extensions/project_extensions.dart';
 import 'package:flower_app/core/extensions/spacer_media_quiey.dart';
+import 'package:flower_app/core/theme/app_colors.dart';
 import 'package:flower_app/core/utils/widgets/custom_button.dart';
 import 'package:flower_app/project_layers/api_layer/models/request/change_password_request_body.dart';
 
@@ -43,93 +44,81 @@ class _ResetPasswordState extends State<ResetPassword> {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => resetPasswordCubit,
-      child:
-          BlocConsumer<
-            ResetPasswordCubit,
-            ResetPasswordState
-          >(
-            builder: (context, state) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: Text(
-                    context.l10n.reset_password,
+      child: BlocConsumer<ResetPasswordCubit, ResetPasswordState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(context.l10n.reset_password),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  ResetPasswordForm(
+                    passwordController:
+                        passwordController,
+                    newPasswordController:
+                        newPasswordController,
+                    formKey: formKey,
                   ),
-                ),
-                body: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      ResetPasswordForm(
-                        passwordController:
-                            passwordController,
-                        newPasswordController:
-                            newPasswordController,
-                        formKey: formKey,
-                      ),
-                      SizedBox(
-                        height: 2.5.heightPercent(
-                          context,
-                        ),
-                      ),
-                      CustomButton(
-                        size: Size(double.infinity, 48),
-                        borderRadius: 30,
-                        child: Text(context.l10n.update),
-                        onPressed: () {
-                          if (formKey.currentState!
-                              .validate()) {
-                            resetPasswordCubit
-                                .changePassword(
-                                  ChangePasswordRequestBody(
-                                    passwordController
-                                        .text,
+                  SizedBox(
+                    height: 2.5.heightPercent(context),
+                  ),
+                  CustomButton(
+                    size: Size(double.infinity, 48),
+                    borderRadius: 30,
+                    child: state is ChangePasswordLoading
+                        ? CircularProgressIndicator.adaptive(
+                            backgroundColor:
+                                AppColors.white,
+                          )
+                        : Text(context.l10n.update),
+                    onPressed: () {
+                      if (formKey.currentState!
+                          .validate()) {
+                        resetPasswordCubit.changePassword(
+                          ChangePasswordRequestBody(
+                            passwordController.text,
 
-                                    newPasswordController
-                                        .text,
-                                  ),
-                                );
-                          }
-                        },
-                      ),
-                    ],
+                            newPasswordController.text,
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        listener: (context, state) {
+          switch (state) {
+            case ChangePasswordLoading():
+              Center(
+                child:
+                    CircularProgressIndicator.adaptive(),
+              );
+              break;
+            case ChangePasswordSuccess():
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    context.l10n.reset_pass_success,
                   ),
                 ),
               );
-            },
-            listener: (context, state) {
-              switch (state) {
-                case ChangePasswordLoading():
-                  Center(
-                    child:
-                        CircularProgressIndicator.adaptive(),
-                  );
-                  break;
-                case ChangePasswordSuccess():
-                  ScaffoldMessenger.of(
-                    context,
-                  ).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                        context.l10n.reset_pass_success,
-                      ),
-                    ),
-                  );
-                  Navigator.pop(context);
-                  break;
-                case ChangePasswordError():
-                  ScaffoldMessenger.of(
-                    context,
-                  ).showSnackBar(
-                    SnackBar(
-                      content: Text(state.message),
-                    ),
-                  );
+              Navigator.pop(context);
+              break;
+            case ChangePasswordError():
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.message)),
+              );
 
-                  break;
-                default:
-              }
-            },
-          ),
+              break;
+            default:
+          }
+        },
+      ),
     );
   }
 }
